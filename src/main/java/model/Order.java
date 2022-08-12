904efceb870d8d0f8e00c75e24b31290f6395389
sonnd <nguyendoanson.sonnd@gmail.com>
2022-08-12 17:14:18 +0700

    Sonnd_init_Model_Order

---
+++ src/main/java/model/Order.java
@@ -0,0 +1,115 @@
+package model;
+
+import java.util.Date;
+
+/*1 customer create 1 order, in order init list orderDetail -
+* is list product order.*/
+public class Order {
+    private int orderId;
+    private String name;
+    private String  phoneNumber;
+    private String detailAddress;
+    private int total;
+    private Date orderDate;
+    private int customerId;
+    private int addressId;
+    private int discountId;
+
+    public Order(String name, String phoneNumber, String detailAddress, int total, Date orderDate, int customerId, int addressId, int discountId) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.detailAddress = detailAddress;
+        this.total = total;
+        this.orderDate = orderDate;
+        this.customerId = customerId;
+        this.addressId = addressId;
+        this.discountId = discountId;
+    }
+
+    public Order(int orderId, String name, String phoneNumber, String detailAddress, int total, Date orderDate, int customerId, int addressId, int discountId) {
+        this.orderId = orderId;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.detailAddress = detailAddress;
+        this.total = total;
+        this.orderDate = orderDate;
+        this.customerId = customerId;
+        this.addressId = addressId;
+        this.discountId = discountId;
+    }
+
+    public Order() {
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDetailAddress() {
+        return detailAddress;
+    }
+
+    public void setDetailAddress(String detailAddress) {
+        this.detailAddress = detailAddress;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public int getDiscountId() {
+        return discountId;
+    }
+
+    public void setDiscountId(int discountId) {
+        this.discountId = discountId;
+    }
+}
---
+++ src/main/java/model/OrderDetail.java
@@ -0,0 +1,10 @@
+package model;
+
+public class OrderDetail {
+    private int cartId;
+    private int quantity;
+    private int total;
+    private int orderId;
+    private int productId;
+
+}
