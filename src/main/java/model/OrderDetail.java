e3ac32591f1d09c8d24a1a3fb1e94b9bde9a9575
sonnd <nguyendoanson.sonnd@gmail.com>
2022-08-13 15:51:19 +0700

    Sonnd Menu Order

--- src/main/java/model/OrderDetail.java
+++ src/main/java/model/OrderDetail.java
@@ -7,4 +7,61 @@ public class OrderDetail {
     private int orderId;
     private int productId;
 
+    public OrderDetail(int quantity, int total, int orderId, int productId) {
+        this.quantity = quantity;
+        this.total = total;
+        this.orderId = orderId;
+        this.productId = productId;
+    }
+
+    public OrderDetail(int cartId, int quantity, int total, int orderId, int productId) {
+        this.cartId = cartId;
+        this.quantity = quantity;
+        this.total = total;
+        this.orderId = orderId;
+        this.productId = productId;
+    }
+
+    public OrderDetail() {
+    }
+
+    public int getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(int cartId) {
+        this.cartId = cartId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
 }
